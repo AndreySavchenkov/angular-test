784c2d959a9e1f09d118189069f0e702c9a2fd27
SavchenkovAndrey <sannndvik16@gmail.com>
2022-10-13 08:52:01 +0300

    first commit

---
+++ src/app/pages/product-page/product-page.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {ModalService} from "src/app/services/modal.service";
+import {ProductsService} from "src/app/services/products.service";
+
+@Component({
+  selector: 'app-product-page',
+  templateUrl: './product-page.component.html'
+})
+export class ProductPageComponent implements OnInit {
+
+  title = 'angular app';
+  loading = false;
+  term = '';
+
+  constructor(
+    public productsService: ProductsService,
+    public modalService: ModalService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.productsService.getAll().subscribe(() => {
+      this.loading = false;
+    })
+  }
+}
